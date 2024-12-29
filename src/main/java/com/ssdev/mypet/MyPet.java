58aa97a39d69043adb86c4bf21d1c57df3726f75
Salomao S. <souza95salomao@gmail.com>
2024-12-29 10:40:24 -0400

    chore :: project bootstraping

---
+++ src/main/java/com/ssdev/mypet/MyPet.java
@@ -0,0 +1,13 @@
+package com.ssdev.mypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPet {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPet.class, args);
+	}
+
+}
