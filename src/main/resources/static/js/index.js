067a114f4a3989e210fca079eac6ba9c08ebd21f
Salomao S. <souza95salomao@gmail.com>
2025-01-02 17:49:47 -0400

    chore :: update templating

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,14 +1,3 @@
-document.getElementById("filter_date")
-  .addEventListener("change", ({ target })=>{
-    window.location.assign(`/?filter=${target.value}`)
-  })
-
-document.getElementById("open-modal-btn")
-  .addEventListener("click", ()=>{
-     document.querySelector(".modal__overlay").remove("hidden")
-  })
-
-document.getElementById("close-modal-btn")
-  .addEventListener("click", ()=>{
-     document.querySelector(".modal__overlay").add("hidden")
-  })
+document.getElementById("filter_date").addEventListener("change", ({ target })=>window.location.assign(`/?filter=${target.value}`))
+document.getElementById("open-modal-btn").addEventListener("click", ()=>document.querySelector(".modal__overlay").classList.remove("hidden"))
+document.getElementById("close-modal-btn").addEventListener("click", ()=>document.querySelector(".modal__overlay").classList.add("hidden"))
