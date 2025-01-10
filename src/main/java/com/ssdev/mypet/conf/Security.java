4852bc1b00973c2cccb3aa432d84353bfa20277c
Salomao S. <souza95salomao@gmail.com>
2025-01-10 16:17:23 -0400

    feat: auth - security config

--- src/main/java/com/ssdev/mypet/conf/Security.java
+++ src/main/java/com/ssdev/mypet/conf/Security.java
@@ -1,16 +1,25 @@
 package com.ssdev.mypet.conf;
 
+import java.security.SecureRandom;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.ssdev.mypet.domain.auth.AuthService;
 
 @Configuration
 public class Security {
+  @Value("${spring.password-encoder-seed}")
+  private String SEED = "";
+
   @Autowired
   private AuthService authService;
 
@@ -23,21 +32,26 @@ public class Security {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-      .csrf((customCsrf) -> customCsrf.disable())
-      .formLogin((customForm) -> customForm
-        .loginPage("/auth/login")
-        .defaultSuccessUrl("/")
-        .permitAll()
-      )
-      .logout((customLogout) -> customLogout
-        .logoutSuccessUrl("/auth/login?logout=true")
-        .deleteCookies("JSESSIONID")
-      )
+      .csrf((csrf) -> csrf.disable())
       .authorizeHttpRequests((req) -> req
         .requestMatchers(staticResources)
         .permitAll()
         .anyRequest()
         .authenticated()
+      )
+      .sessionManagement((session) -> session
+        .maximumSessions(1)
+        .maxSessionsPreventsLogin(true)
+      )
+      .formLogin((form) -> form
+        .loginPage("/login")
+        .defaultSuccessUrl("/")
+        .permitAll()
+      )
+      .logout((logout) -> logout
+        .logoutSuccessUrl("/login?logout=true")
+        .deleteCookies("JSESSIONID")
+        .permitAll()
       );
     return http.build();
   }
@@ -45,5 +59,10 @@ public class Security {
   @Bean
   public UserDetailsService userDetailsService() {
     return (String username)->authService.findByUsername(username).orElseThrow();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(BCryptVersion.$2Y, 16, new SecureRandom(SEED.getBytes()));
   }
 }
