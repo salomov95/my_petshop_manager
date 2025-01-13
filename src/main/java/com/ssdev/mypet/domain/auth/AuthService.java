8ea964be7b2497c6ce3d075e16c6404713f09aee
Salomao S. <souza95salomao@gmail.com>
2025-01-12 21:28:49 -0400

    core: update password encryption on authservice to be a bean for testability improvement

--- src/main/java/com/ssdev/mypet/domain/auth/AuthService.java
+++ src/main/java/com/ssdev/mypet/domain/auth/AuthService.java
@@ -1,34 +1,28 @@
 package com.ssdev.mypet.domain.auth;
 
-import java.security.SecureRandom;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
-  @Value("${spring.password-encoder-seed}")
-  private String SEED = "";
-
   private Logger logger = LoggerFactory.getLogger(AuthService.class);
   private final UserRepository userRepository;
   private final PasswordEncoder encoder;
 
-  public AuthService(UserRepository repository) {
+  public AuthService(UserRepository repository, PasswordEncoder encoder) {
     userRepository = repository;
-    encoder = new BCryptPasswordEncoder(BCryptVersion.$2Y, 16, new SecureRandom(SEED.getBytes()));
+    this.encoder = encoder;
   }
 
   public String createUser(String username) throws Exception {
     try {
-      String passkey = UUID.randomUUID().toString().substring(0, 20);
+      String passkey = generateUUID();
       
       User newUser = new User();
       newUser.setUsername(username);
@@ -56,5 +50,9 @@ public class AuthService {
 
   public Optional<User> findByUsername(String username) {
     return userRepository.findByUsername(username);
+  }
+
+  public String generateUUID() {
+    return UUID.randomUUID().toString().substring(0, 20);
   }
 }
